46308bb87c768ee55cf14b61fd9b8c99ab6f3565
Gyubin Son <geubin0414@gmail.com>
2016-05-28 23:35:27 +0900

    지명이 작업 버전 장고로 옮김

--- tastegroup/urls.py
+++ tastegroup/urls.py
@@ -3,4 +3,6 @@ from . import views     # 이 파일의 현재 디렉토리인 blog 디렉토리
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^list$', views.list, name='list'),
+    url(r'^detail$', views.detail, name='detail'),
 ]
--- tastegroup/views.py
+++ tastegroup/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'tastegroup/index.html')
+
+def list(request):
+    return render(request, 'tastegroup/list.html')
+
+def detail(request):
+    return render(request, 'tastegroup/detail.html')
